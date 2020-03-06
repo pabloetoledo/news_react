3c29132a1656a86b42a6524ae0ba508f0342ef30
unknown <toledopabloesteban@gmail.com>
2020-03-05 22:37:04 -0300

    primera version de la app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
+import Header from './components/Header';
+import ListaNoticias from './components/ListaNoticias';
+import Formulario from './components/Formulario';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { 
+      noticias : []
+   }
+
+  componentDidMount(){
+    this.consultarNoticias();  
+  }
+
+  consultarNoticias = async (categoria = 'general') => {
+    const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=6589dee2f8644d609371d3d3c2371969`;
+    
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+
+    this.setState({
+      noticias : noticias.articles
+    })
+  }
+
+  render() { 
+    return ( 
+      <Fragment>
+        <Header
+          titulo='Noticias REact API'
+        />
+
+        <div className="container white contenedor-noticias">
+          <Formulario
+            consultarNoticias={this.consultarNoticias}
+          />
+          <ListaNoticias
+            noticias={this.state.noticias}
+          />
+        </div>
+
+      </Fragment>
+     );
+  }
 }
-
+ 
 export default App;
---
+++ src/components/Formulario.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Formulario extends Component {
+    state = { 
+        categoria : ''
+    }
+
+    cambiarCategoria = e => {
+        this.setState({
+            categoria : e.target.value
+        }, () => { // utilizamos un call back para que se recargue la pagina despues de cambiar el state
+            //El segundo parametro de un setState puede ser un call back como este...
+            this.props.consultarNoticias(this.state.categoria); 
+        });
+
+        
+    }
+
+    render() { 
+        return ( 
+            <div className="buscador row">
+                <div className="col s12 m8 offset-m2">
+                    <form>
+                        <h2>Encuentra Noticias por Categoria</h2>
+                        <div className="input-field col s12 m8 offset-m2">
+                            <select
+                                onChange ={this.cambiarCategoria}
+                            >
+                                <option value="general">General</option>
+                                <option value="business">Negocios</option>
+                                <option value="entertainment">Entretenimiento</option>
+                                <option value="health">Salud</option>
+                                <option value="science">Ciencia</option>
+                                <option value="sports">Deportes</option>
+                                <option value="technology">Tecnologia</option>
+                            </select>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+         );
+    }
+}
+
+Formulario.propTypes = {
+    consultarNoticias : PropTypes.func.isRequired
+}
+ 
+export default Formulario;
